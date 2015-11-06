46331b0ba5b4959ea030497d0aeac730e4ddf41b
antongulikov <contest.gulikov@gmail.com>
2015-11-06 20:14:25 +0300

    Add click library

---
+++ submitter/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='submitter',
+    version='0.1',
+    py_modules=['submitter'],
+    install_requires=[
+        'Click',
+	'requests',
+    ],
+    entry_points='''
+        [console_scripts]
+        submitter=submitter:main
+    ''',
+)
--- submitter/submitter.py
+++ submitter/submitter.py
@@ -1,4 +1,4 @@
-import argparse
+import click
 import json
 import requests
 import sys
@@ -6,22 +6,55 @@ import time
 
 client_id = "client_id"
 client_secret = "client_secret"
-
-
-auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
-resp = requests.post('https://stepic.org/oauth2/token/', data={'grant_type': 'client_credentials'}, auth=auth)
-token = (resp.json())['access_token']
 stepic_url = "https://stepic.org/api"
-headers = {'Authorization': 'Bearer ' + token, "content-type": "application/json"}
-
-programming_language = {'cpp': 'c++11', 'c': 'c++11', 'py': 'python3',
-                        'java': 'java8', 'hs': 'haskel 7.10', 'sh': 'shell',
-                        'r': 'r'}
-
+token = None
+headers = None
 
 def exit_util(message):
     print(message, file=sys.stderr)
     sys.exit(0)
+
+
+def update_client():
+    global client_id
+    global client_secret
+    global token
+    global headers
+    with open("client_inf", "r") as f:
+        client_id = f.readline()
+        client_id = client_id.split(":")[-1].rstrip()
+        client_secret = f.readline()
+        client_secret = client_secret.split(":")[-1].rstrip()  
+    auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
+    resp = requests.post('https://stepic.org/oauth2/token/', data={'grant_type': 'client_credentials'}, auth=auth)
+    if resp.status_code > 400:
+        exit_util("Lost connection")    
+    token = (resp.json())['access_token']
+    headers = {'Authorization': 'Bearer ' + token, "content-type": "application/json"}
+
+
+programming_language = {'cpp': 'c++11', 'c': 'c++11', 'py': 'python3',
+                        'java': 'java8', 'hs': 'haskel 7.10', 'sh': 'shell',
+                        'r': 'r'}
+                        
+                        
+def set_client(cid, secret):
+    lines = []
+    with open("client_inf", "r") as f:
+        lines = [line.split(":")[-1].rstrip() for line in f]
+    with open("client_inf", "w") as f:
+        f.writelines("clientd_id:{}\n".format(cid or lines[0]))
+        f.writelines("client_secret:{}\n".format(secret or lines[1]))
+        
+
+#def set_secret(csecret):
+ #   lines = []
+  #  with open("client_inf", "r") as f:
+   #     lines = [line for line in f.readlines()]
+    #print(lines)
+#    with open("clinet_inf", "w") as f:
+#        f.writelines(lines[0] + "\n")
+ #       f.writelines("client_secret:{0  }\n".format(csecret))
 
 
 def get_lesson_id(url_parts):
@@ -29,6 +62,7 @@ def get_lesson_id(url_parts):
     for i, part in enumerate(url_parts):
         if part == "lesson" and i + 1 < len_url_parts:
             return int(url_parts[i + 1].split("-")[-1])
+                            
 
 
 def get_step_id(url_parts):
@@ -46,6 +80,7 @@ def get_step_id(url_parts):
 
 
 def set_problem(problem_url):
+    update_client()
     tmp = requests.get(problem_url)
     code = tmp.status_code
     if code >= 500:
@@ -99,6 +134,7 @@ def evaluate(attempt_id):
 
 
 def submit_code(code):
+    update_client()
     file_name = code
     code = "".join(open(code).readlines())
     url = stepic_url + "/submissions"
@@ -127,12 +163,60 @@ def submit_code(code):
     evaluate(submit['submissions'][0]['id'])
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--problem", help="Set problems link", type=str)
-parser.add_argument("--code", help="Send your code to stepic", type=str)
 
-args = parser.parse_args()
-if args.problem:
-    set_problem(args.problem)
-if args.code:
-    submit_code(args.code)
+@click.group()  
+@click.version_option()
+def main():
+    """Submitter 1.0
+       
+       Tools for submitting solutions to stepic.org
+    """    
+    lines = 0
+    try:
+        with open("client_inf", "r") as f:
+            for x in f:
+                lines += 1
+    except Exception as e:
+        pass
+    if lines < 2:
+        with open("client_inf", "w") as f:
+            f.writelines("client_id:\n")
+            f.writelines("client_secret:\n")    
+
+
+
+@main.command()
+@click.option("--link", help="Link to your problem")
+def remember_problem(link=None):
+    """     Rember and Set the current problem.
+
+    """
+    if not link is None:
+        set_problem(link)
+
+
+@main.command()
+@click.option("--solution", help="Path to your solution")
+def submit_action(solution=None):
+    """ Submit a solution.
+
+    """
+    if not solution is None:
+        submit_code(solution)
+
+@main.command()
+@click.option("--cid", help="Your client-id. If you don't have it, please create on https://stepic.org/oauth2/applications/")
+def set_client_id(cid=None):
+    if not cid is None:
+        set_client(cid, None)
+    click.echo("Client id has been changed!")
+        
+
+@main.command()
+@click.option("--csecret", help="Your client-secret. If you don't vae it, please create on https://stepic.org/oauth2/applications/")
+@click.pass_context
+def set_secret(ctx, csecret):
+    if not csecret is None:
+        set_client(None, csecret)    
+    click.echo("Client secret has been changed!")
+
